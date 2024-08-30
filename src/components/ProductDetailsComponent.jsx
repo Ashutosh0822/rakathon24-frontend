3ffdd45ba79562aed28182461e0d5fef4ad10099
sfs1721 <pkssfs1721@gmail.com>
2024-08-30 22:23:05 +0530

    ExperimentPage done

--- src/components/ProductDetailsComponent.jsx
+++ src/components/ProductDetailsComponent.jsx
@@ -1,5 +1,4 @@
 import {
-  Box,
   createTheme,
   Grid2,
   Paper,
@@ -8,53 +7,59 @@ import {
 } from "@mui/material";
 import React from "react";
 
-const ProductDetails = ({ data, handler, theme }) => {
+const ProductDetails = ({ data, handler }) => {
   const darkTheme = createTheme({ palette: { mode: "dark" } });
 
   return (
     <>
-      <ThemeProvider theme={darkTheme}>
-        <Paper
-          elevation={6}
-          sx={{
-            alignSelf: "center",
-            textAlign: "center",
-            width: 90,
-            height: 40,
-            lineHeight: "40px",
-          }}
-        >
-          Product Details
-        </Paper>
-      </ThemeProvider>
       <Grid2
         size={12}
-        sx={{ justifyContent: "center", paddingTop: "2%" }}
-        container
+        sx={{alignItems:'center', justifyItems: "center", paddingTop: "2%" }}
+        // container
         spacing={3}
       >
-        <TextField
-          id="filled-multiline-flexible"
-          label="Product description"
-          name="description"
-          multiline
-          maxRows={4}
-          variant="filled"
-          onChange={(e) =>
-            handler({ ...data, [e.target.name]: e.target.value })
-          }
-        />
-        <TextField
-          id="filled-multiline-flexible"
-          label="Impact to be acheieved"
-          name="impact"
-          multiline
-          maxRows={4}
-          variant="filled"
-          onChange={(e) =>
-            handler({ ...data, [e.target.name]: e.target.value })
-          }
-        />
+        <Grid2>
+          <ThemeProvider theme={darkTheme}>
+            <Paper
+              elevation={6}
+              sx={{
+                alignSelf: "center",
+                textAlign: "center",
+                width: 90,
+                height: 40,
+                lineHeight: "40px",
+              }}
+            >
+              Product Details
+            </Paper>
+          </ThemeProvider>
+        </Grid2>
+        <Grid2 sx={{ justifyItems: "center", paddingTop: "2%" }}>
+          <TextField
+            id="filled-multiline-flexible"
+            label="Product description"
+            name="description"
+            multiline
+            maxRows={4}
+            variant="filled"
+            onChange={(e) =>
+              handler({ ...data, [e.target.name]: e.target.value })
+            }
+          />
+        </Grid2>
+        <Grid2 sx={{ justifyItems: "center", paddingTop: "2%" }}>
+          <TextField
+            id="filled-multiline-flexible"
+            label="Impact to be acheieved"
+            name="impact"
+            multiline
+            maxRows={4}
+            variant="filled"
+            onChange={(e) =>
+              handler({ ...data, [e.target.name]: e.target.value })
+            }
+          />
+        </Grid2>
       </Grid2>
     </>
   );
--- src/pages/ExperimentPage.jsx
+++ src/pages/ExperimentPage.jsx
@@ -1,14 +1,97 @@
 import React, { useState } from "react";
-import Stack from "@mui/material/Stack";
 import ProductDetails from "../components/ProductDetailsComponent";
+import TargetAudience from "../components/TargetAudiene";
+import ContextDetails from "../components/ContextDetailsComponent";
+import { Button, Grid2 } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import axios from "axios";
 
 const ExperimentPage = () => {
-  const [data, handler] = useState({});
+  const nos = [1, 2, 3];
+  const [productData, productHandler] = useState({});
+  const [contextData, contextHander] = useState([
+    {
+      bodyShape: "thin",
+    },
+  ]);
+  const [imageUrls, setImages] = useState([]);
+  const [targetAudienceData, TargetAudienceHandler] = useState({
+    targetAudienceAge: "kid",
+    gender: "female",
+  });
+  const [cnt, updateCount] = useState(1);
+  const submitForm = () => {
+    console.log({ productData, targetAudienceData, contextData });
+    axios
+      .post(`http://localhost:8080/experiment`, {
+        productData,
+        targetAudienceData,
+        contextData,
+      })
+      .then((res) => {
+        setImages(res.data.imageUrls);
+      });
+  };
   return (
-      <Stack sx={{padding:"2%",margin:"1%"}}>
-        {/* {console.log(data)} */}
-        <ProductDetails data={data} handler={handler} sx={{justifyContent:'center'}} />
-      </Stack>
+    <>
+      <Grid2 sx={{ padding: "2%", justifyContent: "center", margin: "1%" }}>
+        <Grid2>
+          <ProductDetails
+            data={productData}
+            handler={productHandler}
+            sx={{ justifyContent: "center" }}
+          />
+          <TargetAudience
+            data={targetAudienceData}
+            handler={TargetAudienceHandler}
+            sx={{ justifyContent: "center" }}
+          />
+        </Grid2>
+        <Grid2>
+          <Grid2 container>
+            {nos
+              .filter((no) => no <= cnt)
+              .map((no) => (
+                <ContextDetails
+                  key={no}
+                  ind={no}
+                  data={contextData}
+                  handler={contextHander}
+                  sx={{ justifyContent: "center" }}
+                />
+              ))}
+            <AddIcon
+              sx={{ padding: 15, margin: 15 }}
+              fontSize="large"
+              onClick={(e) => {
+                e.preventDefault();
+                if (cnt < 3) {
+                  updateCount(cnt + 1);
+                  contextHander([
+                    ...contextData,
+                    {
+                      bodyShape: "thin",
+                    },
+                  ]);
+                }
+              }}
+            />
+          </Grid2>
+        </Grid2>
+        <Grid2>
+          <Button variant="outlined" onClick={() => submitForm()}>
+            Generate Results
+          </Button>
+        </Grid2>
+      </Grid2>
+      <Grid2>
+            {
+              imageUrls.map(
+                url=><img key={url} src={url}/>
+              )
+            }
+      </Grid2>
+    </>
   );
 };
 
